0da6202ad5ea5d5f5a04d93656569901820d323c
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-06-02 13:23:56 +0200

    Test routes created

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/museums", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all museums" });
+});
+
+app.post("/api/v1/museums", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create a new museum" });
+});
+
+app.get("/api/v1/museums/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Get museum ${req.params.id}` });
+});
+
+app.put("/api/v1/museums/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update museum ${req.params.id}` });
+});
+
+app.delete("/api/v1/museums/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete museum ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
